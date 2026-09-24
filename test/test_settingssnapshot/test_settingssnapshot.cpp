b0c1225dd23699d4950f069d9dac685ab7532311
martinh87 <m.heissl@gmail.com>
2026-09-24 22:34:14 +0200

    Fix JSON string in test_defaults_serialize_whole

--- test/test_settingssnapshot/test_settingssnapshot.cpp
+++ test/test_settingssnapshot/test_settingssnapshot.cpp
@@ -57,8 +57,8 @@ static void test_defaults_serialize_whole() {
   TEST_ASSERT_EQUAL_STRING(
       "{\"autoBrightness\":false,\"brightness\":120,\"autoTransition\":true,\"textColor\":"
       "\"#FFFFFF\",\"transitionEffect\":\"Rain\",\"transitionDirection\":\"normal\","
-      "\"transitionDurationMs\":1000,\"appDurationMs\":7000,\"timeMode\":1,
-      \"indicatorStyle\":0,\"calendarHeaderColor\":\"#FF0000\",\"calendarTextColor\":\"#000000\","
+      "\"transitionDurationMs\":1000,\"appDurationMs\":7000,\"timeMode\":1,"
+      "\indicatorStyle\":0,\"calendarHeaderColor\":\"#FF0000\",\"calendarTextColor\":\"#000000\","
       "\"calendarBodyColor\":\"#FFFFFF\",\"time24h\":true,\"timeLeadingZero\":true,"
       "\"timeShowSeconds\":false,\"timeShowAmPm\":false,\"timeSeparatorMode\":\"pulse\","
       "\"dateOrder\":\"dayMonthYear\",\"dateSeparator\":\"dot\",\"dateYearMode\":\"twoDigit\","
