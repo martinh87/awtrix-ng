5b1ce29ac229ff29708d35532ef38640d3aad046
martinh87 <m.heissl@gmail.com>
2026-09-24 22:46:06 +0200

    Fix formatting in test_settingssnapshot.cpp

--- test/test_settingssnapshot/test_settingssnapshot.cpp
+++ test/test_settingssnapshot/test_settingssnapshot.cpp
@@ -58,7 +58,7 @@ static void test_defaults_serialize_whole() {
       "{\"autoBrightness\":false,\"brightness\":120,\"autoTransition\":true,\"textColor\":"
       "\"#FFFFFF\",\"transitionEffect\":\"Rain\",\"transitionDirection\":\"normal\","
       "\"transitionDurationMs\":1000,\"appDurationMs\":7000,\"timeMode\":1,"
-      "\indicatorStyle\":0,\"calendarHeaderColor\":\"#FF0000\",\"calendarTextColor\":\"#000000\","
+      "\"indicatorStyle\":0,\"calendarHeaderColor\":\"#FF0000\",\"calendarTextColor\":\"#000000\","
       "\"calendarBodyColor\":\"#FFFFFF\",\"time24h\":true,\"timeLeadingZero\":true,"
       "\"timeShowSeconds\":false,\"timeShowAmPm\":false,\"timeSeparatorMode\":\"pulse\","
       "\"dateOrder\":\"dayMonthYear\",\"dateSeparator\":\"dot\",\"dateYearMode\":\"twoDigit\","
